4b70c0904a4478bbfbe1eeaade1b9694b67de5ca
HyperPigeon <58276133+HyperPigeon@users.noreply.github.com>
2023-02-19 15:50:12 -0500

    changed code to use hudrendercallback and text renderer, since using sendMessage was causing some weird flickering on MCC island

--- src/main/java/net/hyper_pigeon/issprinton/client/IsSprintOnClient.java
+++ src/main/java/net/hyper_pigeon/issprinton/client/IsSprintOnClient.java
@@ -4,18 +4,36 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.text.Text;
 
 @Environment(EnvType.CLIENT)
 public class IsSprintOnClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        ClientTickEvents.END_CLIENT_TICK.register(
-                client -> {
-                    if(client.player != null && client.player.isSprinting()){
-                        client.player.sendMessage(Text.translatable("SPRINTING"), true);
-                    }
+//        ClientTickEvents.END_CLIENT_TICK.register(
+//                client -> {
+//                    if(client.player != null && client.player.isSprinting()){
+//                        client.player.sendMessage(Text.translatable("SPRINTING"), true);
+//                    }
+//                }
+//        );
+        HudRenderCallback.EVENT.register((matrixStack, tickDelta) -> {
+            TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
+            int scaledWidth = MinecraftClient.getInstance().getWindow().getScaledWidth();
+            int scaledHeight = MinecraftClient.getInstance().getWindow().getScaledHeight();
+
+            if(MinecraftClient.getInstance().player != null) {
+                if(MinecraftClient.getInstance().player.isSprinting()){
+                    textRenderer.drawWithShadow(matrixStack, "SPRINTING", ((float)scaledWidth/2)-20, scaledHeight - 60, 0xFFFFFF);
                 }
-        );
+                else {
+                    textRenderer.drawWithShadow(matrixStack, "WALKING", ((float)scaledWidth/2)-20, scaledHeight - 60, 0xFFFFFF);
+                }
+            }
+
+        });
     }
 }
